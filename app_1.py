80fef0fe07000aaef21ce888b21b695e2627bc19
pink <66056077@kmitl.ac.th>
2023-10-29 11:24:35 +0700

    analyze tree

--- app_1.py
+++ app_1.py
@@ -15,3 +15,16 @@ with col2:
     st.write('Column2')
 with col3:
     st.write('Column3')
+
+trees_df = pd.read_csv('trees.csv')
+df_dbh_grouped = pd.DataFrame(
+    #สร้างกราฟโดย group จาก column dbh แล้วนับจำนวน tree_id (นับจำนวนต้นไม้)
+    trees_df.groupby(['dbh']).count()['tree_id'])
+df_dbh_grouped.colunms = ['tree_count']
+st.line_chart(df_dbh_grouped)
+
+st.caption('กราฟแสดงจำนวนต้นไม้ จัดกบุ่มตามเส้นผ่านศูนย์กลาง')
+st.title('แปลผล')
+st.write("""
+  ส่วนใหญ่ของต้นไม้ใน sf มีเส้นผ่าศูนย์กลาง 3' (2721 ต้น)
+""")
